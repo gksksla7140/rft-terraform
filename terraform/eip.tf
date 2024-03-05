b554da42dddd2d16434675080525484daf1f1be7
Justin Han <gksksla7140@gmail.com>
2024-03-05 15:33:12 -0800

    format files

--- terraform/eip.tf
+++ terraform/eip.tf
@@ -1,14 +1,14 @@
 resource "aws_eip" "eip_1" {
-  depends_on = [ aws_internet_gateway.igw ]
+  depends_on = [aws_internet_gateway.igw]
   tags = {
     Name = "${var.app_name}-eip-1"
   }
 }
 
 resource "aws_eip" "epi_2" {
-    depends_on = [ aws_internet_gateway.igw ]
-    tags = {
-        Name = "${var.app_name}-eip-2"
-    }
-  
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "${var.app_name}-eip-2"
+  }
+
 }
--- terraform/ngw.tf
+++ terraform/ngw.tf
@@ -1,9 +1,9 @@
 resource "aws_nat_gateway" "ngw1" {
   allocation_id = aws_eip.eip_1.id
-  subnet_id = aws_subnet.public_1.id
+  subnet_id     = aws_subnet.public_1.id
 
-  depends_on = [ aws_internet_gateway.igw ]
-  
+  depends_on = [aws_internet_gateway.igw]
+
   tags = {
     Name = "${var.app_name}-ngw1"
   }
@@ -11,10 +11,10 @@ resource "aws_nat_gateway" "ngw1" {
 
 resource "aws_nat_gateway" "ngw2" {
   allocation_id = aws_eip.eip_2.id
-  subnet_id = aws_subnet.public_2
+  subnet_id     = aws_subnet.public_2
 
-  depends_on = [ aws_internet_gateway.igw ]
-  
+  depends_on = [aws_internet_gateway.igw]
+
   tags = {
     Name = "${var.app_name}-ngw2"
   }
