126d3502b4264e7f2a40604115eb098130a9590d
Justin Han <gksksla7140@gmail.com>
2024-03-05 15:51:02 -0800

    Fix subnet_id assignment in ngw2 resource

--- terraform/ngw.tf
+++ terraform/ngw.tf
@@ -11,7 +11,7 @@ resource "aws_nat_gateway" "ngw1" {
 
 resource "aws_nat_gateway" "ngw2" {
   allocation_id = aws_eip.eip_2.id
-  subnet_id     = aws_subnet.public_2
+  subnet_id     = aws_subnet.public_2.id
 
   depends_on = [aws_internet_gateway.igw]
 
