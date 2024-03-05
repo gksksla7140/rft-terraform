79e2b0c519cf511526d13152ba32bda5ec1cf398
Justin Han <gksksla7140@gmail.com>
2024-03-05 15:39:24 -0800

    Fix resource name and reference in eip.tf and ngw.tf

--- terraform/eip.tf
+++ terraform/eip.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "eip_1" {
   }
 }
 
-resource "aws_eip" "epi_2" {
+resource "aws_eip" "eip_2" {
   depends_on = [aws_internet_gateway.igw]
   tags = {
     Name = "${var.app_name}-eip-2"
