ba3854142989731c187aecd819b418b5adf150b6
Justin Han <gksksla7140@gmail.com>
2024-03-05 13:52:12 -0800

    Update AWS region variable and formatting in main.tf and variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,22 @@
-variable aws_region {}
+variable "aws_region" {}
 
 variable "app_name" {
-    description = "The name of the application"
-    type        = string
-    default = "rft-terraform"
+  description = "The name of the application"
+  type        = string
+  default     = "rft-terraform"
 }
 
 variable "public_subnet" {
-    default = ["10.10.100.0/24", "10.10.101.0/24"]
-    type = list(string)
+  default = ["10.10.100.0/24", "10.10.101.0/24"]
+  type    = list(string)
 }
 
 variable "private_subnet" {
-    default = ["10.10.0.0/24", "10.10.1.0/24"]
-    type = list(string)
+  default = ["10.10.0.0/24", "10.10.1.0/24"]
+  type    = list(string)
 }
 
 variable "availability_zones" {
-    default = ["us-west-2a", "us-west-2b"]
-    type = list(string)
+  default = ["us-west-2a", "us-west-2b"]
+  type    = list(string)
 }
