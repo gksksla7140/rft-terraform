6b4e3a7b908c055f430400006cf09e5e5a86d0b6
Justin Han <gksksla7140@gmail.com>
2024-03-05 15:37:56 -0800

    Fix allocation_id in aws_nat_gateway resource

--- terraform/ngw.tf
+++ terraform/ngw.tf
@@ -10,7 +10,7 @@ resource "aws_nat_gateway" "ngw1" {
 }
 
 resource "aws_nat_gateway" "ngw2" {
-  allocation_id = aws_eip.eip_2.id
+  allocation_id = aws_eip.eip.eip_2.id
   subnet_id     = aws_subnet.public_2
 
   depends_on = [aws_internet_gateway.igw]
